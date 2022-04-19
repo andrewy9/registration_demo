2ad2825aa35bdd1913dcc70460a6029ca16b0c35
andrewy9 <andrew.yang993@gmail.com>
2022-04-19 16:36:03 +1200

    changed student_sequence to user_sequence

--- src/main/java/com/andrew/registration_demo/appuser/AppUser.java
+++ src/main/java/com/andrew/registration_demo/appuser/AppUser.java
@@ -19,46 +19,43 @@ import java.util.Collections;
 @Entity
 public class AppUser implements UserDetails {
 
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "student_sequence"
-    )
-    @Id
     @SequenceGenerator(
             name = "student_sequence",
             sequenceName = "student_sequence",
             allocationSize = 1
     )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
-    private String name;
-    private String username;
+    private String firstName;
+    private String lastName;
     private String email;
     private String password;
     @Enumerated(EnumType.STRING)
     private AppUserRole appUserRole;
-    private Boolean locked;
-    private Boolean enabled;
+    private Boolean locked = false;
+    private Boolean enabled = false;
 
-    public AppUser(String name,
-                   String username,
+    public AppUser(String firstName,
+                   String lastName,
                    String email,
                    String password,
-                   AppUserRole appUserRole,
-                   Boolean locked,
-                   Boolean enabled) {
-        this.name = name;
-        this.username = username;
+                   AppUserRole appUserRole) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
         this.appUserRole = appUserRole;
-        this.locked = locked;
-        this.enabled = enabled;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         SimpleGrantedAuthority authority =
                 new SimpleGrantedAuthority(appUserRole.name());
+        System.out.println("singletone list: " + Collections.singletonList(authority));
         return Collections.singletonList(authority);
     }
 
@@ -69,7 +66,11 @@ public class AppUser implements UserDetails {
 
     @Override
     public String getUsername() {
-        return this.username;
+        return this.email;
+    }
+
+    public String getLastName() {
+        return this.lastName;
     }
 
     @Override
